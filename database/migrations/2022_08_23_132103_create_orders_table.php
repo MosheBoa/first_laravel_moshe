bf321c63518dba9d7fc4adb23d39012fc9191eeb
MosheBoa <moshe@boaideas.com>
2022-08-24 10:40:04 +0300

    first_commit

---
+++ database/migrations/2022_08_23_132103_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string("shopify_order_numbers");
+            $table->string("customer_full_name");
+            $table->dateTime("order_date");
+            $table->decimal("order_summary_amount");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2022_08_23_134026_create_line_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('line_items', function (Blueprint $table) {
+            $table->id();
+            $table->string("line_items_title");
+            $table->decimal("price_after_discount");
+            $table->dateTime("line_item_sku");
+            $table->decimal("line_item_id");
+            $table->decimal("line_item_barcode");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('line_items');
+    }
+};
